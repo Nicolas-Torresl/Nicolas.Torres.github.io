32cafb7f534aeb1f6ea28ed1fe1d628ac0c76caf
Nicolas-Torresl <cristorropez@gmail.com>
2026-02-03 17:05:08 -0500

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: `https://NicolasTorres.github.io`,
+  site: "https://NicolasTorres.github.io",
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
